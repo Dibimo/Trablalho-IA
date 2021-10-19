481951298f2c3b8a7355a63533d479ccc12dd3bf
Dibimo <diogobissolimoreno@gmail.com>
2021-10-19 11:14:55 -0300

    .\main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from astar_python.astar import Astar
-from Utilitarios import verifica_valores, receber_valores
+from Utilitarios import converter_para_hexadecimal, retorna_cores, verifica_valores, receber_valores
 import matplotlib.pyplot as plt
 import networkx as nx
 
@@ -9,8 +9,8 @@ def main():
     # 0 siguinifica custo 0 para percorrer
     # maiores valores implicam em desviar a rota, buscando um custo menor
     mat = [
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -5],
+        [5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -27,7 +27,7 @@ def main():
 
     mapa = Astar(mat)
     caminho = mapa.run([x0, y0], [x, y])
-
+    cores_dos_nodos = retorna_cores(caminho, [105, 245, 112])
     ################# gráfico #########################
     G = nx.grid_2d_graph(11, 10)
 
@@ -41,7 +41,7 @@ def main():
         for y in range(9)
     ], weight=1.4)
 
-    plt.figure(figsize=(6, 6))
+    plt.figure(figsize=(10, 10))
     pos = {(x, y): (x, -y) for x, y in G.nodes()}
 
     ################# Pintar o caminho correto com cor diferente ################
@@ -50,16 +50,15 @@ def main():
         color = "orange"
         for ponto in caminho:
             if verifica_valores(ponto,node):
-                
-                color = "lightgreen"
-
+                color = cores_dos_nodos[node]
+                color = converter_para_hexadecimal(color)
         color_map.append(color)
     
     
     nx.draw(G, pos=pos,
             node_color=color_map,
-            with_labels=True,
-            node_size=600)
+            with_labels=False,
+            node_size=1000)
     plt.show()
 
 main()
