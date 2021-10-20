dca1cfcefe57dfef9d0d6e23dd095556e6cc74e2
Dibimo <diogobissolimoreno@gmail.com>
2021-10-20 20:18:39 -0300

    Pequenas melhorias

--- main.py
+++ main.py
@@ -8,19 +8,33 @@ LARGURA_MATRIZ = 11
 ALTURA_MATRIZ = 10
 
 def main():
+    # Atribuição das matrizes de pesos e nomes
     matriz_de_pesos = Matrizes.matriz_de_pesos
     nomes = Matrizes.nomes
 
     ################# A* Rodando ######################
 
-    x0, y0, x, y = receber_valores(LARGURA_MATRIZ,ALTURA_MATRIZ)
+    # Recebendo valores
+    x0, y0, x, y = receber_valores(LARGURA_MATRIZ - 1,ALTURA_MATRIZ - 1)
 
+    # Gerando mapa de pontos
     mapa = Astar(matriz_de_pesos)
+
+    # Encontrando o caminho entre os pontos estipulados
     caminho = mapa.run([x0, y0], [x, y])
 
     ################# gráfico #########################
+    # Gerando gráfico vazio
     grafico = nx.grid_2d_graph(LARGURA_MATRIZ, ALTURA_MATRIZ)
 
+
+    # Criando janela
+    plt.figure(figsize=(10, 10))
+
+    # Gerando nodos
+    posicoes = {(x, y): (x, -y) for x, y in grafico.nodes()}
+
+    # Adicionando as linhas na vertical entre os nodos
     grafico.add_edges_from([
         ((x, y), (x+1, y+1))
         for x in range(LARGURA_MATRIZ - 1)
@@ -31,18 +45,19 @@ def main():
         for y in range(ALTURA_MATRIZ - 1)
     ], weight=1.4)
 
-    plt.figure(figsize=(10, 10))
-    posicoes = {(x, y): (x, -y) for x, y in grafico.nodes()}
-
-    ################# Pintar o caminho correto com cor diferente ################
-    lista_de_cores = retorna_lista_cores(grafico,matriz_de_pesos,caminho,[105, 245, 112])
+    ################# Nomeando nodos ################
     nomes_nodos = retorna_lista_labels(grafico,nomes)
+
+    ################# Colorindo gráfico ################
+    lista_de_cores = retorna_lista_cores(grafico,matriz_de_pesos,caminho,[105, 245, 112])
     
+    # Desenhando gráfico
     nx.draw(grafico, pos=posicoes,
             node_color=lista_de_cores,
             with_labels=False,
             node_size=1000)
     
+    # Desenhando nomes dos nodos
     nx.draw_networkx_labels(grafico,posicoes,labels=nomes_nodos)
     plt.show()
 
